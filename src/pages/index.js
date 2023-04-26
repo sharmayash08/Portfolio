09e9bda3c16c2b63fb48aee672a518d75f3c0e82
Yash Sharma <110451168+sharmayash08@users.noreply.github.com>
2023-04-26 20:12:54 +0530

    Changed Index.js and Added New CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,14 +35,14 @@ export default function Home() {
             <p className='my-4 text-base font-medium md:text-sm sm:text-sm'>As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
       Explore my latest projects and articles, showcasing my expertise in React.js and web development.</p>
       <div className='flex items-center self-start mt-2 lg:self-center'>
-        <Link href="/CV.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light  dark:text-dark
+        <Link href="/YashCV.pdf" target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light  dark:text-dark
         hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base
         "
         //download={true}
         >Resume
         <LinkArrow className="w-6 ml-1" />
         </Link>
-        <Link href="maito:yash280876@gmail.com" target={"_blank"}
+        <Link href="mailto:yash280876@gmail.com" target={"_blank"}
         className="ml-4 text-lg font-medium capitalize text-dark underline dark:text-light md:text-base"
         >Contact</Link>
       </div>
