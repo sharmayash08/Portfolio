d7dfb4d4ae87914c2d58119afedb20b849e93858
Yash Sharma <yash280876@gmail.com>
2024-02-13 20:11:09 +0530

    Updated icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,11 +82,10 @@ const Navbar = () => {
         </nav>
         
         <nav className='flex items-center justify-center flex-wrap'>
-            <motion.a href="https://twitter.com/yash280876" target={"_blank"} whileHover = {{y:-5}} whileTap={{scale:0.9}} className="w-6 mr-3"><TwitterIcon /></motion.a>
+            
             <motion.a href="https://github.com/sharmayash08" target={"_blank"}whileHover = {{y:-5}} whileTap={{scale:0.9}} className="w-6 mx-3"><GithubIcon /></motion.a>
             <motion.a href="https://www.linkedin.com/in/yashsharma0801" target={"_blank"}whileHover = {{y:-5}} whileTap={{scale:0.9}} className="w-6 mx-3"><LinkedInIcon /></motion.a>
-            <motion.a href="https://in.pinterest.com/yash280876/" target={"_blank"}whileHover = {{y:-5}} whileTap={{scale:0.9}} className="w-6 mx-3"><PinterestIcon /></motion.a>
-            <motion.a href="https://dribbble.com/yash080105" target={"_blank"}whileHover = {{y:-5}} whileTap={{scale:0.9}} className="w-6 mx-3"><DribbbleIcon /></motion.a>
+            
 
             <button onClick={() => setMode(mode==="light" ? "dark" : "light")} className="ml-3 flex items-center justify-center rounded-full p-1">
               {
