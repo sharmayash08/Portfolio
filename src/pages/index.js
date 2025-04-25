30adbe64a97177c0873242e6c6cdcf93d8420f01
Yash Sharma <110451168+sharmayash08@users.noreply.github.com>
2025-04-25 09:03:35 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
             xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:text-3xl
             ' />
             <p className='my-4 text-base font-medium md:text-sm sm:text-sm'>
-              As a skilled Software Engineer, I am dedicated to turning ideas into innovative web applications. 
+              As an Aspiring Software Engineer, I am dedicated to turning ideas into innovative web applications. 
               Explore my latest projects and articles, showcasing my expertise in React.js and web development.
             </p>
             <div className='flex items-center self-start mt-2 lg:self-center'>
